11ff5c2371e41ed58074434be1da5f2ad1a3f495
vxyzs <varunsriram10@gmail.com>
2024-11-04 13:10:48 +0530

    updates made

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -85,9 +85,11 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'users',
-        'USER': 'root',
+        'NAME': os.getenv('DB_NAME'),
+        'USER': os.getenv('DB_USER'),
         'PASSWORD': os.getenv('DB_PASSWORD'),
+        'HOST': os.getenv('DB_HOST'),
+        'PORT': os.getenv('DB_PORT'),
     }
 }
 
